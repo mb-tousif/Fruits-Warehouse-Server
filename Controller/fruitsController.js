f3dec5ebbc8cf0ca04a36dbf144473f3c93533db
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-06 13:43:36 +0600

    Update API created

--- Controller/fruitsController.js
+++ Controller/fruitsController.js
@@ -1,5 +1,6 @@
 import freshFruit from "../Model/fruitSchema.js"
 
+// Get All Fruits API
 export const getFruits = async (req, res) => {
   try {
     const fruits = await freshFruit.find({});
@@ -9,13 +10,27 @@ export const getFruits = async (req, res) => {
   }
 };
 
+// Get Single Fruit API 
 export const getFruit = async (req, res) => {
   try {
     const id = req.params.id
-    console.log(id);
+    // console.log(id);
     const fruit = await freshFruit.findOne({_id : id});
     res.status(200).json(fruit);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Update Single Fruit Data API
+export const updateField = async (req, res)=>{
+  try {
+    const id = req.params.id;
+    const quantity = req.body;
+    console.log(quantity);
+    const updatedFruit = await freshFruit.updateOne({_id: id}, { $set: quantity})
+    res.status(200).json(updatedFruit);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
--- Router/Route.js
+++ Router/Route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {getFruit, getFruits} from "../Controller/fruitsController.js";
+import {getFruit, getFruits, updateField} from "../Controller/fruitsController.js";
 
 const router = express.Router();
 
 router.get("/fruits", getFruits);
 router.get("/fruit/:id", getFruit);
+router.put("/fruit/:id", updateField);
 
 
 export default router;
