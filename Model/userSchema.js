dbda3a5db55a25f9986beda1010ddb7e65225f7c
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-09 22:01:40 +0600

    User Updated

--- Model/userSchema.js
+++ Model/userSchema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const userSchema = new mongoose.Schema({
   id: String,
   email: String,
-  role: String,
+  role: { type: String, default: "user" },
   img: String,
 });
 
